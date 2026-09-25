9900a8486c08714bfdb8e02da7a34cf198affa14
akolasia <172996930+aik38@users.noreply.github.com>
2026-09-25 20:28:02 +0900

    Connect production GAS endpoint

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.VC_CONFIG = {
   // Google Apps Script Web App URL. Deploy gas/Code.gs, then set the URL here.
-  GAS_ENDPOINT: "",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbxWid5uttybIOiYspUy4H--tlyn76GbJ1_AX7Jhc_W7Sx4kaatVS5TYmypK_bAtnHpF/exec",
   // Turnstile site key. Leave empty until Cloudflare setup is complete.
   TURNSTILE_SITE_KEY: "",
   // GA4 measurement ID. Leave empty until dedicated VALUE CAPITAL property is created.
